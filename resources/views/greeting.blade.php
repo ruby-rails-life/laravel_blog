5f1b833bf819cc5d48794f7f08239b1bab7463f5
ruby-rails-life <rubyrails.life@gmail.com>
2017-11-17 16:13:54 +0900

    template

--- resources/views/greeting.blade.php
+++ resources/views/greeting.blade.php
@@ -1,5 +1,13 @@
-<html>
-    <body>
-        <h1>Hello, {{ $name }}</h1>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    @@parent
+
+    <p>ここはメインのサイドバーに追加される</p>
+@endsection
+
+@section('content')
+    <p>Hello, {{ $name }}</p>
+@endsection
